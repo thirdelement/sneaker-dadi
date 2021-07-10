3d81aafd5a85a8da724e4c810bfeedfb9fa9c546
thirdelement <gamechanger@Hotmail.co.uk>
2021-07-10 07:28:41 +0000

    feat: In views.py add parameter, view and context for gender.

--- products/views.py
+++ products/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
+
 
 def all_products(request):
     """" A view to show all products, including sorting and search queries. """
 
     products = Product.objects.all()
     query = None
+    categories = None
+    gender = None
 
     if request.GET:
+        
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories) 
+            categories = Category.objects.filter(name__in=categories)
+            
+        if 'gender' in request.GET:
+            gender = request.GET['gender'].split(',')
+            products = products.filter(gender__in=gender)
+            gender = Product.objects.filter(gender__in=gender)  
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +37,9 @@ def all_products(request):
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_gender': gender, 
     }
 
     return render(request, 'products/products.html', context)
