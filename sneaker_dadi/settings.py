203a2495123b7b5eff58089ab0847c5ffcf2a3ef
thirdelement <gamechanger@Hotmail.co.uk>
2021-07-20 18:52:48 +0000

    build: Install django-crispy-forms and add config to settings.py.

--- sneaker_dadi/settings.py
+++ sneaker_dadi/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'cart',
     'multiselectfield',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'sneaker_dadi.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
