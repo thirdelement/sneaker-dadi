700d3adc4257bfa41fe3b067e4e471099ad371be
thirdelement <gamechanger@Hotmail.co.uk>
2021-07-25 08:17:43 +0000

    feat: Add view & url to check if the save info box selected.

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,28 @@ from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        # split the client secret to obtain the payment intent id and store in variable pid
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        # set up stripe with the secret key so we can modify the payment intent
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        # call stripe.PaymentIntent.modify and tell it what we want to modify
+        # add user placing order, if they wanted to save their info & JSON dump of cart
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
