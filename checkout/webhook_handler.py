e546853fd022c91e839ff683122bc15b3fe02fec
thirdelement <gamechanger@Hotmail.co.uk>
2021-07-28 18:01:08 +0000

    feat: Updated webhook_handler.py for profiles.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
-# from profiles.models import UserProfile
+from profiles.models import UserProfile
 
 import json
 import time
@@ -42,6 +42,26 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+        
+        # Update profile information if save_info was checked
+        # Make profile = None to create order for anonymous users
+        profile = None
+        username = intent.metadata.username
+        # If username is not anonymous then they are authenticated
+        if username != 'AnonymousUser':
+            # Attempt to get profile with username from metadata
+            profile = UserProfile.objects.get(user__username=username)
+            # if save info box is checked
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                # save profile
+                profile.save()
         
         # If order does not already exist in the db
         order_exists = False
@@ -86,7 +106,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    # user_profile=profile,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
