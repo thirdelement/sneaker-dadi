1a75cc480854b4f8fd799110a282e1b505fa64f8
thirdelement <gamechanger@Hotmail.co.uk>
2021-07-29 18:10:42 +0000

    style: Remove styling for form fields.

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .widgets import CustomClearableFileInput
+# from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -9,7 +9,7 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    # image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,5 +17,5 @@ class ProductForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+        # for field_name, field in self.fields.items():
+           # field.widget.attrs['class'] = 'border-black rounded-0'
