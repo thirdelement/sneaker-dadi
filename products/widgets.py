b43941b6c2559fed4c637f00eb06678caccb4af8
thirdelement <gamechanger@Hotmail.co.uk>
2021-07-31 09:12:12 +0000

    style: Update label text in widgets.py.

--- products/widgets.py
+++ products/widgets.py
@@ -5,6 +5,6 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
-    initial_text = _('Current Image')
+    initial_text = _('Current image')
     input_text = _('')
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
