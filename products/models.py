97d1cfd937005d644c4867df355a18ae0f499984
thirdelement <gamechanger@Hotmail.co.uk>
2021-08-14 19:14:27 +0000

    feat: Add fields, get_sale_price & clean methods to Product model.

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from multiselectfield import MultiSelectField
+from decimal import Decimal
 
 
 # Create your models here.
@@ -35,8 +36,10 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    sale_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    discount = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    # sale_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    on_sale = models.BooleanField(default=False)
+    # discount = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    discount_percent = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     size = MultiSelectField(choices=SIZE_CHOICES)
     gender = models.CharField(max_length=1)
@@ -44,6 +47,22 @@ class Product(models.Model):
     image2 = models.ImageField(null=True, blank=True)
     image3 = models.ImageField(null=True, blank=True)
     image4 = models.ImageField(null=True, blank=True)
-
+     
     def __str__(self):
         return self.name
+  
+    def get_sale_price(self):
+        '''Calculate cost with discount percentage
+        Credit: https://helperbyte.com/questions/77886/django-how-to-make-a-discount-for-the-item
+        '''
+        sale = Decimal(self.price * (100 - self.discount_percent) / 100).quantize(Decimal('0.00'))
+        return sale
+    
+    def clean(self):
+        '''Raise validation error if on_sale is True and discount_percent field is empty 
+        Credit: https://stackoverflow.com/questions/13440097/django-modelform-booleanfield-required-field-is-not-working
+        '''
+        from django.core.exceptions import ValidationError
+        if self.on_sale and not self.discount_percent:
+            raise ValidationError("Discount percent is a required field.")
+        
