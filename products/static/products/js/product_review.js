0ec1f1ad9121260a93bda6453fc06ead8017e0d2
thirdelement <gamechanger@Hotmail.co.uk>
2021-08-22 21:22:00 +0000

    feat: Update product_detail.html & JS to show review text correctly.

--- products/static/products/js/product_review.js
+++ products/static/products/js/product_review.js
@@ -1,44 +1,41 @@
 // Product Review Save
 // Credit: Code Artisan Lab - https://www.youtube.com/watch?v=7tyMyLCjKVg&list=PLgnySyq8qZmrxJvJbZC1eb7PD4bu0a-sB&index=31
-$("#addForm").submit(function(e){
+$("#addForm").submit(function (e) {
 	$.ajax({
-		data:$(this).serialize(),
-		method:$(this).attr('method'),
-		url:$(this).attr('action'),
-		dataType:'json',
-		success:function(res){
-			if(res.bool==true){
-				$(".ajaxRes").html('Review has been added.');
+		data: $(this).serialize(),
+		method: $(this).attr('method'),
+		url: $(this).attr('action'),
+		dataType: 'json',
+		success: function (res) {
+			if (res.bool == true) {
+				// $(".ajaxRes").html('Review has been added.');
 				// Reset form button
 				$("#reset").trigger('click');
-				console.log("test")
 				// Hide Add Review button
 				$("#btn-review").hide();
-				// End
-
-				//// create data for review
-				//var _html='<blockquote class="blockquote text-right">';
-				//_html+='<small>'+res.data.review_text+'</small>';
-				//_html+='<footer class="blockquote-footer">'+res.data.user;
-				//_html+='<cite title="Source Title">';
-				//for(var i=1; i<=res.data.review_rating; i++){
-				//	_html+='<i class="fa fa-star text-warning"></i>';
-				//}
-				//_html+='</cite>';
-				//_html+='</footer>';
-				//_html+='</blockquote>';
-				//_html+='</hr>';
-
-				//$(".no-data").hide();
-
-				//// Prepend Data
-				//$(".review-list").prepend(_html);
-
-				//// Hide Modal
-				//$("#productReview").modal('hide');
-
-				//// AVg Rating
-				//$(".avg-rating").text(res.avg_reviews.avg_rating.toFixed(1))
+				// create data for review
+				var _html = '<blockquote class="blockquote text-right">';
+				_html += '<cite title="Source Title">';
+				for (var i = 1; i <= res.data.review_rating; i++) {
+					_html += '<i class="bi bi-star-fill text-warning"></i>';
+				}
+				_html += '</cite>';
+				_html += '<p>' + res.data.review_text + '</p>';
+				// Credit for first letter capitalize below: https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+				_html += '<footer class="blockquote-footer">'+ res.data.user.charAt(0).toUpperCase()+res.data.user.slice(1) 
+				_html += ', ';
+				_html += '<span>' + res.data.created_on;
+				_html += '</footer>';
+				_html += '</blockquote>';
+				_html += '</hr>';
+				// Hide Add First Review text
+				$(".no-data").hide();
+				// Prepend Data
+				$(".review-list").prepend(_html);
+				// Hide Modal
+				$("#productReview").modal('hide');
+				// Avg Rating
+				$(".avg-rating").text(res.avg_reviews.avg_rating.toFixed(1))
 			}
 		}
 	});
