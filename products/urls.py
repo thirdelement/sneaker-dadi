12aa94dd7a3ffc363bfeceb7ca54056fdf7f5257
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-01 06:24:26 +0000

    feat: Create products_on_sale.html.

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('products_on_sale/', views.products_on_sale, name='products_on_sale'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('save-review/<int:product_id>/', views.save_review, name='save-review'),
     path('delete-review/<int:review_id>/', views.delete_review, name='delete-review'),
+    # path('edit-review/<int:review_id>/', views.edit_review, name='edit-review'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
