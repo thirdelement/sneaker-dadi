e9aaf3dfe781cf59bd12fa593080cf22ddacc686
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-08 17:18:13 +0000

    refactor: Remove commented out code from settings.py.

--- sneaker_dadi/settings.py
+++ sneaker_dadi/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-bgvcgn@03mf*km!yw_ac7*#o=$12#1s8i*do9lsop*$zakrm-o'
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -131,11 +130,14 @@ if 'DATABASE_URL' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+           'ENGINE': 'django.db.backends.sqlite3',
+           'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
+# DATABASES = {
+#     'default': dj_database_url.parse('postgres://hlzvcltepzkfdv:d529d7c4ff308804e151c14630e8c023a6fb71f9e0a0b1649975367d88cc8424@ec2-54-73-152-36.eu-west-1.compute.amazonaws.com:5432/d5hfngr8h5uesb')
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
