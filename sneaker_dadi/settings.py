a5ac9d7ff68400ddad17ac05b27efed0b152b778
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-09 17:18:41 +0000

    fix: Update email settings in settings.py.

--- sneaker_dadi/settings.py
+++ sneaker_dadi/settings.py
@@ -208,16 +208,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
+# STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'dadi@sneaker-dadi.com'
 
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'sneakerdadi@flashy-shoes.com'
+    DEFAULT_FROM_EMAIL = 'dadi@sneaker-dadi.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
