dce0939b36c0b5c99c19a78b7da015351ae0589e
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-14 20:07:27 +0000

    style: Add Flickity carousel to index.html.

--- home/views.py
+++ home/views.py
@@ -9,9 +9,9 @@ from django.db.models import F
 def top_sellers(request):
     # Most sold male product
     # Credit: https://stackoverflow.com/questions/9278796/ordering-a-query-by-aggregate-sum-in-django-but-not-getting-result-as-expected
-    top_sellers_male = Product.objects.filter(gender='m').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:2]
+    top_sellers_male = Product.objects.filter(gender='m').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:3]
     # Most sold female product
-    top_sellers_female = Product.objects.filter(gender='f').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:2]
+    top_sellers_female = Product.objects.filter(gender='f').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:3]
     # Most recently sold male product
     trending_male = Product.objects.filter(gender='m').order_by(F('orderlineitem__order__date').desc(nulls_last=True))
     # Get top two distinct products
