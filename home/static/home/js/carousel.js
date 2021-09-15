f4eb33e657b0e8571ab27df3d7792b7de533b3b2
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-15 20:09:01 +0000

    style: Add media queries for carousel and JS to resize cell height.

---
+++ home/static/home/js/carousel.js
@@ -0,0 +1,26 @@
+
+
+// Credit SirNovi post at https://github.com/metafizzy/flickity/issues/534
+// Function to resize carousel cards to same height
+function setSliderHeightToMax(slider) {
+  slider.cells.forEach(cell => cell.element.style.height = '');
+      
+  let heights = slider.cells.map(cell => cell.element.offsetHeight),
+      max = Math.max.apply(Math, heights);
+  
+  slider.cells.forEach(cell => cell.element.style.height = max + 'px');
+}
+
+// Run function above on ready and on window resize
+var slider = document.querySelector('.carousel');
+var flkty = new Flickity( slider, {
+    on: {
+        ready: function() {
+            setSliderHeightToMax(this);
+        }
+    }
+});
+
+window.addEventListener('resize', function(){
+    setSliderHeightToMax(flkty);
+});
