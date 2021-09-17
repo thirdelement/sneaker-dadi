b726ae43a3b0ffb6b6b04704ed3b66f505f46476
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-17 16:44:46 +0000

    feat: Add Flickity carousel for product trending query on index.html.

--- home/views.py
+++ home/views.py
@@ -14,7 +14,7 @@ def top_sellers(request):
     top_sellers_female = Product.objects.filter(gender='f').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:3]
     # Most recently sold male product
     trending_male = Product.objects.filter(gender='m').order_by(F('orderlineitem__order__date').desc(nulls_last=True))
-    # Get top two distinct products
+    # Get top three distinct products
     # Credit: https://stackoverflow.com/questions/19283225/how-can-i-remove-duplicate-objects-in-an-order-by-query
     male_trender = set()
     male_trend_distinct = []
@@ -22,17 +22,17 @@ def top_sellers(request):
         if item not in male_trender:
             male_trend_distinct.append(item)
             male_trender.add(item)
-    trending_male = male_trend_distinct[:2]
+    trending_male = male_trend_distinct[:3]
     # Most recently sold female product
     trending_female = Product.objects.filter(gender='f').order_by(F('orderlineitem__order__date').desc(nulls_last=True))
-    # Get top two distinct products
+    # Get top three distinct products
     fem_trender = set()
     fem_trend_distinct = []
     for item in trending_female:
         if item not in fem_trender:
             fem_trend_distinct.append(item)
             fem_trender.add(item)
-    trending_female = fem_trend_distinct[:2]
+    trending_female = fem_trend_distinct[:3]
    
     context = {
         'top_sellers_male': top_sellers_male,
