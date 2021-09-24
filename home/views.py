54fbf60a81a8ddfc1d24da98d1f631dd016446a7
thirdelement <gamechanger@Hotmail.co.uk>
2021-09-24 11:21:39 +0000

    fix: Resolve PEP8 errors E501 on home/views.py.

--- home/views.py
+++ home/views.py
@@ -7,15 +7,29 @@ from django.db.models import F
 
 # Top two selling products for male and female
 def top_sellers(request):
-    # Most sold male product
-    # Credit: https://stackoverflow.com/questions/9278796/ordering-a-query-by-aggregate-sum-in-django-but-not-getting-result-as-expected
-    top_sellers_male = Product.objects.filter(gender='m').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:3]
+    '''
+    Most sold male product
+    Credit: https://stackoverflow.com/questions/9278796/
+    ordering-a-query-by-aggregate-sum-in-django-but-not-getting-result-as-expected
+    '''
+    top_sellers_male = Product.objects.filter(
+        gender='m').annotate(total_quantity=Sum(
+            'orderlineitem__quantity')).order_by(F(
+                'total_quantity').desc(nulls_last=True))[:3]
     # Most sold female product
-    top_sellers_female = Product.objects.filter(gender='f').annotate(total_quantity=Sum('orderlineitem__quantity')).order_by(F('total_quantity').desc(nulls_last=True))[:3]
+    top_sellers_female = Product.objects.filter(
+        gender='f').annotate(total_quantity=Sum(
+            'orderlineitem__quantity')).order_by(F(
+                'total_quantity').desc(nulls_last=True))[:3]
     # Most recently sold male product
-    trending_male = Product.objects.filter(gender='m').order_by(F('orderlineitem__order__date').desc(nulls_last=True))
-    # Get top three distinct products
-    # Credit: https://stackoverflow.com/questions/19283225/how-can-i-remove-duplicate-objects-in-an-order-by-query
+    trending_male = Product.objects.filter(
+        gender='m').order_by(F(
+            'orderlineitem__order__date').desc(nulls_last=True))
+    '''
+    Get top three distinct products
+    Credit: https://stackoverflow.com/questions/19283225/
+    how-can-i-remove-duplicate-objects-in-an-order-by-query
+    '''
     male_trender = set()
     male_trend_distinct = []
     for item in trending_male:
@@ -24,7 +38,9 @@ def top_sellers(request):
             male_trender.add(item)
     trending_male = male_trend_distinct[:3]
     # Most recently sold female product
-    trending_female = Product.objects.filter(gender='f').order_by(F('orderlineitem__order__date').desc(nulls_last=True))
+    trending_female = Product.objects.filter(
+        gender='f').order_by(F(
+            'orderlineitem__order__date').desc(nulls_last=True))
     # Get top three distinct products
     fem_trender = set()
     fem_trend_distinct = []
